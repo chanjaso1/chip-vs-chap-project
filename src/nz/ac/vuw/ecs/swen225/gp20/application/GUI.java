5344733d2c228914dbeef2f567b0ecd0f2c3fef5
Bea <jaymejoan@ecs.vuw.ac.nz>
2020-09-16 17:46:30 +1200

    First GUI Layout Commit
    This commit contains the first draft layout of the GUI. All the main sections have been divided and the menu bar created.

--- src/nz/ac/vuw/ecs/swen225/gp20/application/GUI.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/application/GUI.java
@@ -1,9 +1,6 @@
 package nz.ac.vuw.ecs.swen225.gp20.application;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.GridLayout;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
@@ -13,20 +10,7 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseWheelEvent;
 import java.io.File;
 
-import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JSplitPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
+import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.text.DefaultCaret;
 
@@ -35,15 +19,15 @@ import javax.swing.text.DefaultCaret;
  * The GUI displays the time left to play, the current level, keys collected
  * and the number of treasures that still need to be connected.
  * The player can interact with the GUI through keystrokes.
- *
  */
 public abstract class GUI {
 
     private JFrame frame;
 
 
-    public GUI(){
-
+    public GUI() {
+        initialise();
+        System.out.println("in constructor");
     }
 
 
@@ -51,14 +35,70 @@ public abstract class GUI {
      * Initialises and displays the GUI on the screen.
      */
     public void initialise() {
+
+        // creates main frame
         frame = new JFrame("Chip's Challenge");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1400, 850);
+
+
+        // creates menu bar components
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu saveButton = new JMenu("SAVE");
+        JMenu pauseButton = new JMenu("PAUSE");
+        JMenu resumeButton = new JMenu("RESUME");
+        JMenu replayButton = new JMenu("REPLAY");
+        JMenu helpButton = new JMenu("HELP");
+        JMenu exitButton = new JMenu("EXIT");
+
+
+        // adds components to menu bar
+        menuBar.add(saveButton);
+        menuBar.add(pauseButton);
+        menuBar.add(resumeButton);
+        menuBar.add(replayButton);
+        menuBar.add(helpButton);
+        menuBar.add(exitButton);
+
+        // creates board panel
+        JPanel bodyPanel = new JPanel(new GridLayout(1, 2));
+
+        // creates board panel
+        JComponent board = new JPanel();    //TODO: replace with standard's
+        board.setLayout(new BoxLayout(board, BoxLayout.Y_AXIS));
+        board.setBorder(BorderFactory.createEmptyBorder(15, 10, 10, 10));
+        board.setBackground(Color.GRAY);
+        board.add(new JButton("board"));
+
+
+        // creates game stats panel
+        JPanel gameStats = new JPanel();
+        gameStats.setBorder(BorderFactory.createEmptyBorder(15, 10, 10, 10));
+        gameStats.setBackground(Color.darkGray);
+        gameStats.add(new JButton("game stats"));
+
+        bodyPanel.add(board);
+        bodyPanel.add(gameStats);
+
+
+        // add all components to the main frame
+        frame.getContentPane().add(BorderLayout.NORTH, menuBar);
+        frame.getContentPane().add(BorderLayout.CENTER, bodyPanel);
+//        frame.getContentPane().add(BorderLayout.EAST, gameStats);
+        frame.setVisible(true);
+        System.out.println("here");
 
 
     }
 
+}
+
+class Dummy extends GUI {
 
 
-
-
+    public static void main(String... args) {
+        new Dummy();
+    }
 }
 
