5a05c6eca069043915420e5dc613e60241c3b454
Bea <jaymejoan@ecs.vuw.ac.nz>
2020-09-16 16:41:45 +1200

    Initial Application commit
    Initial commit which creates the constructor for the class.

--- src/nz/ac/vuw/ecs/swen225/gp20/application/GUI.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/application/GUI.java
@@ -1,4 +1,64 @@
 package nz.ac.vuw.ecs.swen225.gp20.application;
 
-public class GUI {
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+import java.io.File;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.text.DefaultCaret;
+
+/**
+ * This class constructs and displays the GUI for the game.
+ * The GUI displays the time left to play, the current level, keys collected
+ * and the number of treasures that still need to be connected.
+ * The player can interact with the GUI through keystrokes.
+ *
+ */
+public abstract class GUI {
+
+    private JFrame frame;
+
+
+    public GUI(){
+
+    }
+
+
+    /**
+     * Initialises and displays the GUI on the screen.
+     */
+    public void initialise() {
+        frame = new JFrame("Chip's Challenge");
+
+
+    }
+
+
+
+
+
 }
+
