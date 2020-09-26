2821fc4044156faed24ba42132746b2c6ab6dc1e
John Eleigio Cecilio <cecilijohn@ecs.vuw.ac.nz>
2020-09-26 12:48:40 +1200

    transferred functions into methods for RendererPanel
    Callable functions

--- src/nz/ac/vuw/ecs/swen225/gp20/render/Pane.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/render/Pane.java
@@ -9,17 +9,6 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Pane {
 
-    public int[][] board = new int[100][100];
-    public Tile[][] tileMap = new Tile[100][100];
-    int xPos, yPos;
-    int frameWidth, frameHeight;
-
-    // 0 - North
-    // 1 - East
-    // 2 - South
-    // 3 - West
-    int direction = 2;
-
     Image north, south, east, west;
 
     public Pane() {
@@ -30,58 +19,11 @@ public class Pane {
         east = new ImageIcon(getClass().getResource("resource/rightFacing.gif")).getImage();
         west = new ImageIcon(getClass().getResource("resource/leftFacing.gif")).getImage();
 
-        int num;
-        // Initialize 2D Array with random SHIT.
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 100; j++) {
-                num = ThreadLocalRandom.current().nextInt(0, 3 + 1);
-                board[i][j] = num;
-            }
-        }
-
-        // Convert int 2D array into tileMap 2D Array
-        int tileType;
-        Tile tile = null;
-
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 100; j++) {
-                tileType = board[i][j];
-                switch (tileType) {
-                    case 0:
-                        tile = new Blue(i, j);
-                        break;
-                    case 1:
-                        tile = new Red(i, j);
-                        break;
-                    case 2:
-                        tile = new Grey(i, j);
-                        break;
-                    case 3:
-                        tile = new Yellow(i, j);
-                        break;
-                }
-                tileMap[i][j] = tile;
-            }
-        }
-
         JFrame frame = new JFrame("Tile Map Test");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1000, 1000);
-        frameHeight = (frame.getHeight() /2) - 50;
-        frameWidth = (frame.getWidth() /2) - 50;
 
-        xPos = 25;
-        yPos = 25;
-
-       JComponent drawBoard = new JComponent() {
-            public void paintComponent(Graphics g) {
-                Graphics2D g2d = (Graphics2D) g;
-                g2d.fillRect(0, 0, this.getWidth(), this.getHeight());
-                renderTiles(g2d);
-                drawPlayer(g2d, this);
-                // if greater than 0-24 or 0-25 don't accept.
-            }
-        };
+       RendererPanel drawBoard = new RendererPanel();
 
         frame.addKeyListener(new KeyListener() {
             @Override
@@ -95,48 +37,18 @@ public class Pane {
             @Override
             public void keyReleased(KeyEvent keyEvent) {
 
-                // KEY NOTE:
-                // Each of these should instead just be public call-able methods accessible
-                // to application key listener
-
                 if(keyEvent.getKeyCode()== KeyEvent.VK_RIGHT) {
-                    if (xPos > 40) System.out.println("Boundary Reached");
-                    else {
-                        System.out.println("Right");
-                        xPos++;
-                        System.out.println(xPos + " " + yPos);
-                        direction = 1;
-                    }
+                    drawBoard.renderMove(1);
                 }
                 else if(keyEvent.getKeyCode()== KeyEvent.VK_LEFT) {
-                    if (xPos < 10) System.out.println("Boundary Reached");
-                    else {
-                        System.out.println("Left");
-                        xPos--;
-                        System.out.println(xPos + " " + yPos);
-                        direction = 3;
-                    }
+                    drawBoard.renderMove(3);
                 }
                 else if(keyEvent.getKeyCode()== KeyEvent.VK_DOWN) {
-                    if (yPos > 40) System.out.println("Boundary Reached");
-                    else {
-                        System.out.println("Down");
-                        yPos++;
-                        System.out.println(xPos + " " + yPos);
-                        direction = 2;
-                    }
+                    drawBoard.renderMove(2);
                 }
                 else if(keyEvent.getKeyCode()== KeyEvent.VK_UP) {
-                    if (yPos < 10) System.out.println("Boundary Reached");
-                    else {
-                        System.out.println("Up");
-                        yPos--;
-                        System.out.println(xPos + " " + yPos);
-                        direction = 0;
-                    }
+                    drawBoard.renderMove(0);
                 }
-
-                drawBoard.repaint();
             }
         });
 
@@ -144,44 +56,6 @@ public class Pane {
        frame.setVisible(true);
 
     }
-
-    public void renderTiles(Graphics2D g) {
-        for (int i = 0; i < 5; i++) {
-            int invert = i*-1;
-            drawRow(g, i);
-            drawRow(g, invert);
-        }
-    }
-
-    public void drawRow(Graphics2D g, int y) {
-        // Centre Tile
-
-        tileMap[y + yPos][xPos].drawTile(g, frameWidth, frameHeight + (y * 100));
-
-        for (int i = 1; i < 5; i++) {
-            tileMap[y + yPos][xPos - i].drawTile(g, frameWidth - (i * 100), frameHeight + (y * 100));
-            tileMap[y + yPos][xPos + i].drawTile(g, frameWidth + (i * 100), frameHeight + (y * 100));
-        }
-    }
-
-    public void drawPlayer(Graphics2D g, JComponent display) {
-        switch (direction) {
-            case 0: // North
-                g.drawImage(north, frameWidth, frameHeight, display);
-                break;
-            case 1: //East
-                g.drawImage(east, frameWidth, frameHeight, display);
-                break;
-            case 2: // South
-                g.drawImage(south, frameWidth, frameHeight, display);
-                break;
-            case 3: // West
-                g.drawImage(west, frameWidth, frameHeight, display);
-                break;
-        }
-    }
-
-
 
     public static void main(String... args) {
         new Pane();
--- src/nz/ac/vuw/ecs/swen225/gp20/render/RendererPanel.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/render/RendererPanel.java
@@ -2,14 +2,20 @@ package nz.ac.vuw.ecs.swen225.gp20.render;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class RendererPanel extends JComponent {
 
     public int[][] board = new int[100][100];
     public Tile[][] tileMap = new Tile[100][100];
-    int xPos, yPos;
-    int frameWidth, frameHeight;
+    public int xPos, yPos;
+    public int frameWidth, frameHeight;
 
+    // 0 - North
+    // 1 - East
+    // 2 - South
+    // 3 - West
     int direction = 2;
 
     Image north, south, east, west;
@@ -20,6 +26,46 @@ public class RendererPanel extends JComponent {
 
         xPos = 25;
         yPos = 25;
+
+        // Initialize character facing GIFS
+        north = new ImageIcon(getClass().getResource("resource/backFacing.gif")).getImage();
+        south = new ImageIcon(getClass().getResource("resource/frontFacing.gif")).getImage();
+        east = new ImageIcon(getClass().getResource("resource/rightFacing.gif")).getImage();
+        west = new ImageIcon(getClass().getResource("resource/leftFacing.gif")).getImage();
+
+        int num;
+        // Initialize 2D Array with random SHIT.
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                num = ThreadLocalRandom.current().nextInt(0, 3 + 1);
+                board[i][j] = num;
+            }
+        }
+
+        // Convert int 2D array into tileMap 2D Array
+        int tileType;
+        Tile tile = null;
+
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                tileType = board[i][j];
+                switch (tileType) {
+                    case 0:
+                        tile = new Blue(i, j);
+                        break;
+                    case 1:
+                        tile = new Red(i, j);
+                        break;
+                    case 2:
+                        tile = new Grey(i, j);
+                        break;
+                    case 3:
+                        tile = new Yellow(i, j);
+                        break;
+                }
+                tileMap[i][j] = tile;
+            }
+        }
     }
 
     public void paintComponent(Graphics g) {
@@ -66,4 +112,44 @@ public class RendererPanel extends JComponent {
         }
     }
 
+    public void renderMove(int dir) {
+        if(dir == 1) {
+            if (xPos > 40) System.out.println("Boundary Reached");
+            else {
+                System.out.println("Right");
+                xPos++;
+                System.out.println(xPos + " " + yPos);
+                direction = 1;
+            }
+        }
+        else if(dir == 3) {
+            if (xPos < 10) System.out.println("Boundary Reached");
+            else {
+                System.out.println("Left");
+                xPos--;
+                System.out.println(xPos + " " + yPos);
+                direction = 3;
+            }
+        }
+        else if(dir == 2) {
+            if (yPos > 40) System.out.println("Boundary Reached");
+            else {
+                System.out.println("Down");
+                yPos++;
+                System.out.println(xPos + " " + yPos);
+                direction = 2;
+            }
+        }
+        else if(dir == 0) {
+            if (yPos < 10) System.out.println("Boundary Reached");
+            else {
+                System.out.println("Up");
+                yPos--;
+                System.out.println(xPos + " " + yPos);
+                direction = 0;
+            }
+        }
+
+        this.repaint();
+    }
 }
