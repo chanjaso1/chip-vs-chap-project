8209820ac60daef9d90e0db1c435edf0172ba667
John Eleigio Cecilio <cecilijohn@ecs.vuw.ac.nz>
2020-10-15 16:54:51 +1300

    added comments to SoundEffect

--- src/nz/ac/vuw/ecs/swen225/gp20/render/SoundEffect.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/render/SoundEffect.java
@@ -5,9 +5,16 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import java.io.File;
 
+/**
+ * This class is for the handling of sound effects. Sound effects usually occur after certain events.
+ */
 public class SoundEffect {
     Clip clip;
 
+    /**
+     * For initialising the sound effect .wav file into the object.
+     * @param filename the name of the sound effect .wav file to play
+     */
     public void setFile(String filename) {
         try {
             File file = new File(filename);
@@ -19,6 +26,9 @@ public class SoundEffect {
         }
     }
 
+    /**
+     * Method that plays the sound effect when called.
+     */
     public void playSound() {
         clip.stop();
         clip.setFramePosition(0);
