d010e423cf6937ad645a3a5bd45d2d2716940a23
Bea <jaymejoan@ecs.vuw.ac.nz>
2020-10-18 23:17:13 +1300

    Removed redundant code in GUI
    Removed redundant code and defined the final tasks to complete

--- src/nz/ac/vuw/ecs/swen225/gp20/application/GUI.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/application/GUI.java
@@ -260,6 +260,8 @@ public abstract class GUI {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("new game at unfinished");
+
+                //TODO: Implement code
             }
         });
 
@@ -269,6 +271,7 @@ public abstract class GUI {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("level 1");
+                //TODO: Implement code
             }
         });
 
@@ -302,71 +305,21 @@ public abstract class GUI {
      * This includes displaying the current level, time, chips left and inventory.
      */
     public void displayGameStatsPanel(JPanel gameStats) {
-//        gameStats = new JPanel(new GridLayout(5, 1, 10, 30));
         gameStats.setBorder(BorderFactory.createEmptyBorder(50, 90, 50, 90));
-//        gameStats.setBackground(Color.GRAY);
-
 
         // level panel
         JPanel levelPanel = createGameStat("LEVEL", 1);
-      /*  JPanel levelPanel = new JPanel(layout);
-        JLabel levelTitle = new JLabel("LEVEL", JLabel.CENTER);
-        levelTitle.setFont(TITLE_FONT);
-        levelTitle.setForeground(Color.RED);
-
-        JLabel level = new JLabel("1", JLabel.CENTER);
-        formatStat(level, border);
-
-        levelPanel.add(levelTitle);
-        levelPanel.add(level); */
-
 
         // time panel
         JPanel timePanel = displayTimePanel();
 
-
-      /*  JPanel timePanel = new JPanel(layout);
-        JLabel timeTitle = new JLabel("TIME", JLabel.CENTER);
-        timeTitle.setFont(TITLE_FONT);
-        timeTitle.setForeground(Color.RED);
-
-        JLabel time = new JLabel("60", JLabel.CENTER);
-        formatStat(time, border);
-
-        timePanel.add(timeTitle);
-        timePanel.add(time); */
-
         // treasures left panel
         JPanel keysLeftPanel = createGameStat("TREASURES\nLEFT", treasures);
-      /*  JPanel keysLeftPanel = new JPanel(layout);
-        JLabel keysLeftTitle = new JLabel("CHIPS\nLEFT", JLabel.CENTER);
-        keysLeftTitle.setFont(TITLE_FONT);
-        keysLeftTitle.setForeground(Color.RED);
-
-        JLabel chips = new JLabel("2", JLabel.CENTER);
-        formatStat(chips, border);
-
-        keysLeftPanel.add(keysLeftTitle);
-        keysLeftPanel.add(chips); */
 
         // inventory (keys collected) panel
         JPanel inventoryPanel = createGameStat("KEYS COLLECTED", keysCollected);
-//        JPanel inventoryPanel = new JPanel(GAME_STATS_LAYOUT);
-//        JLabel inventoryTitle = new JLabel("KEYS COLLECTED", JLabel.CENTER);
-//        inventoryTitle.setFont(TITLE_FONT);
-//        inventoryTitle.setForeground(Color.RED);
-//
-//        JLabel inventory = new JLabel(String.valueOf(keys), JLabel.CENTER);
-//        inventory.setBorder(GAME_STATS_BORDER);
-//        inventory.setFont(TITLE_FONT);
-//        inventory.setOpaque(true);
-//        inventory.setBackground(Color.darkGray);
-//        inventory.setForeground(Color.WHITE);
 //
 //        //TODO: draw the actual keys
-//
-//        inventoryPanel.add(inventoryTitle);
-//        inventoryPanel.add(inventory);
 
 
         // add all components to frame
