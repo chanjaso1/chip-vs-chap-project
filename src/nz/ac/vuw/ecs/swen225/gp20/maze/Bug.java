bd51a1e056d294422f61998178185baaea04a813
Phoebe Khokgawe <khokgawep@gmail.com>
2020-10-20 00:26:27 +1300

    Null pointer exception is solved due to the wrong order declaration.

--- src/nz/ac/vuw/ecs/swen225/gp20/maze/Bug.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/maze/Bug.java
@@ -1,10 +1,16 @@
 package nz.ac.vuw.ecs.swen225.gp20.maze;
 
 public class Bug extends Actor {
+    private boolean moveDown = true;
+
     public Bug(int row, int col) {
         super(row, col);
     }
 
+    public void moveBug(){
+        if(moveDown) //MOVE DOWN AND CHECK IF TILE IS GOOD
+            this.getGame()
+    }
 
     @Override
     public String toString() {
--- src/nz/ac/vuw/ecs/swen225/gp20/maze/Player.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/maze/Player.java
@@ -69,6 +69,7 @@ public class Player extends Actor {
      */
     public void moveToNextLevel(){
         level++;
+
     }
 
     //@Override
