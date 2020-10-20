41b57b8a7db35a16e84fad4410f7c4f46fa3958e
Phoebe Khokgawe <khokgawep@gmail.com>
2020-10-21 12:36:50 +1300

    The public constructor is added to the recharge tile.

--- src/nz/ac/vuw/ecs/swen225/gp20/maze/rechargeTile.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/maze/rechargeTile.java
@@ -1,6 +1,17 @@
 package nz.ac.vuw.ecs.swen225.gp20.maze;
 
 public class rechargeTile implements Tile {
+    private int row,col;
+
+    /**
+     * Recharge tile constructor.
+     * @param row -- the tile row position.
+     * @param col -- the tile column position.
+     */
+    public rechargeTile(int row, int col){
+        this.row = row;
+        this.col = col;
+    }
 
     @Override
     public boolean checkValidMove(Player player) {
